c9d532755d1a0facf20fe99bec812e44da750d76
Zac Krasnow <TheOnlyZac@gmail.com>
2017-10-15 15:35:24 -0400

    fixed style problems for PEP-8

--- story.py
+++ story.py
@@ -6,17 +6,17 @@ class StdObject():
     """ Base object from which all other objects derive from.
     Shoule never be used directly, only derived from. """
 
-    def __init__(self, name, longDesc="", shortDesc=""):
+    def __init__(self, name, longDesc = "", shortDesc = ""):
         self.name = name
         self.longDesc = longDesc
         self.shortDesc = shortDesc
-    
+
     def __str__(self):
         return "A StdObject called '{0}'".format(self.name)
 
     def __repr__(self):
         return "Name: {0}\nlongDesc: {1}\nshortDesc: {2}"\
-            "\n".format(self.name, self.longDesc, self.shortDesc)
+               "\n".format(self.name, self.longDesc, self.shortDesc)
 
 
 class Living(StdObject):
@@ -24,19 +24,19 @@ class Living(StdObject):
     Should never be used directly, only derived from. """
 
     def __init__(self, name, longDesc = "", shortDesc = "",
-                gender = "x", race = "human"):
+                 gender = "x", race = "human"):
         super().__init__(name, longDesc, shortDesc)
         self.gender = gender
         self.race = race
         self.inventory = Container("{0}'s Inventory".format(self.name))
-    
+
     def __str__(self):
         return "A {0} {1} (Living) named '{2}'".format(self.get_gender(), self.race, self.name)
 
     def __repr__(self):
         return "Name: {0}\nlongDesc: {1}\nshortDesc: {2}\ngender: {3}\nrace: {4}".format(
-                    self.name, self.longDesc, self.shortDesc, self.gender, self.race)
-    
+               self.name, self.longDesc, self.shortDesc, self.gender, self.race)
+
     def get_gender(self):
         """ Returns 'Male', 'Female', or an empty string
         depending on the Living's gender (m/f/x). """
@@ -53,7 +53,7 @@ class Living(StdObject):
         """ Inserts the given item into this Living's inventory. """
 
         self.inventory.insert(item)
-    
+
     def has_item(self, item) -> bool:
         """ Returns whether or not the Living currently has
         the specified item (true/false). """
@@ -63,17 +63,17 @@ class Living(StdObject):
 
 class Player(Living):
     """ Controls the Player and handles interaction. """
-    
+
     def __init__(self, name, longDesc= "", shortDesc = "",
-                gender = "x", race = "human"):
+                 gender = "x", race = "human"):
         super().__init__(name, longDesc, shortDesc, gender, race)
-    
+
     def __str__(self):
         return "A {0} {1} named {2}".format(self.get_gender(), self.race, self.name)
 
     def __repr__(self):
         return "Name: {0}\nlongDesc: {1}\nshortDesc: {2}\ngender: {3}\nrace: {4}".format(
-                    self.name, self.longDesc, self.shortDesc, self.gender, self.race)
+            self.name, self.longDesc, self.shortDesc, self.gender, self.race)
 
 
 class Item(StdObject):
@@ -81,7 +81,7 @@ class Item(StdObject):
 
     def __init__(self, name, longDesc="", shortDesc=""):
         super().__init__(name, longDesc, shortDesc)
-    
+
     def __str__(self):
         return "A generic Item called '{0}'".format(self.name)
 
@@ -96,14 +96,14 @@ class Container(Item):
     def __init__(self, name, longDesc="", shortDesc=""):
         super().__init__(name, longDesc, shortDesc)
         self.inventory = []
-    
+
     def __str__(self):
         return "A Container called '{0}'".format(self.name)
 
     def __repr__(self):
         return "Name: {0}\nlongDesc: {1}\nshortDesc: {2}"\
-            "\n".format(self.name, self.longDesc, self.shortDesc)
-    
+               "\n".format(self.name, self.longDesc, self.shortDesc)
+
     def show_contents(self):
         """ Prints the name of the container followed by
         it's contents, each in a new line. """
@@ -116,10 +116,11 @@ class Container(Item):
         """ Inserts the given item into the Container's inventory """
 
         self.inventory.append(item)
-    
+
     def has_item(self, item) -> bool:
         """ Returns whether or not the Container currently contains
         the specified item. """
+
         return item in self.inventory
 
     def transfer_to(self, other, item):
@@ -140,14 +141,14 @@ class Location(StdObject):
 class Exit(Item):
     """ Exits handle moving the Player from one Location to another. """
 
-    def __init__(self, name):
-        self.name = name
+    def __init__(self, name, longDesc, shortDesc):
+        super().__init__(self, name, longDesc, shortDesc)
         self.destination = None
 
-        
+
 
 def test_suite():
-    testStdObject = StdObject("object", "a nonspecific generic object",  "a generic object")
+    testStdObject = StdObject("object", "a nonspecific generic object", "a generic object")
     testLiving = Living("living thing", "a living, breathing being", "a living thing")
     testPlayer = Player("Zac", "the star of the show", "you")
     testItem = Item("item", "just a random item lying around", "an item")
@@ -171,3 +172,4 @@ def test_suite():
     testPlayer.inventory.show_contents()
 
 test_suite()
+print(testStdObject)
