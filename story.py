0a45020bdd577992560eae828a93712a1623cec9
Neil Goldader <avatarneil@gmail.com>
2017-10-14 23:22:15 -0400

    Began building framework for Living class

--- story.py
+++ story.py
@@ -3,17 +3,26 @@ import numpy
 
 class StdObject():
 
-    def __init__(self,name,longDesc="",shortDesc=""):
+    def __init__(self, name,longDesc="", shortDesc=""):
         self.name = name
         self.longDesc = longDesc
         self.shortDesc = shortDesc
     
     def __str__(self):
-        return "Name: {0}\nlongDesc: {1}\nshortDesc: {2}\n".format(self.name,self.longDesc,self.shortDesc)
+        return "Name: {0}\nlongDesc: {1}\nshortDesc: {2}\n".format(self.name,
+        self.longDesc,self.shortDesc)
+
+class Living(StdObject):
+    
+    def __init__(self, name, longDesc = "", shortDesc = "", gender = "Attack Helicopter", race = "Zamboni"):
+        super().__init__(name, longDesc, shortDesc)
+        self.gender = gender
+        self.race = race
+
+    def __str__(self):
+        return "Name: {0}\ngender: {1}\nrace: {2}\nlongDesc: {3}\n"\
+        "shortDesc: {4}\n".format(self.name,self.gender,self.race,self.longDesc,self.shortDesc)
 
 
-#class Living(StdObject):
-
-test = StdObject("Zac","An absolute nerd","Nerd")
-
-print(test)
+person = Living("Zac")
+print(person)
