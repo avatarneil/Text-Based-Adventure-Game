a7b6c2907b5a5a6c38697b58ce307e9af9a378cb
Neil Goldader <avatarneil@gmail.com>
2017-10-14 22:55:01 -0400

    Began building StdObject

--- story.py
+++ story.py
@@ -1,3 +1,19 @@
 import sys
 import numpy
 
+class StdObject():
+
+    def __init__(self,name,longDesc="",shortDesc=""):
+        self.name = name
+        self.longDesc = longDesc
+        self.shortDesc = shortDesc
+    
+    def __str__(self):
+        return "Name: {0}\nlongDesc: {1}\nshortDesc: {2}\n".format(self.name,self.longDesc,self.shortDesc)
+
+
+#class Living(StdObject):
+
+test = StdObject("Zac","An absolute nerd","Nerd")
+
+print(test)
