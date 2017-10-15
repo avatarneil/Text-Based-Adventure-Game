c1ecd4ee7b00d217678923decb067ec3e9a225ac
Neil Goldader <avatarneil@gmail.com>
2017-10-14 23:36:11 -0400

    Resolved more duplicates

--- story.py
+++ story.py
@@ -1,43 +1,47 @@
 import sys
 import numpy
 
+
 class StdObject():
 
     def __init__(self, name, longDesc="", shortDesc=""):
         self.name = name
         self.longDesc = longDesc
         self.shortDesc = shortDesc
-    
+
     def __str__(self):
         return "Name: {0}\nlongDesc: {1}\nshortDesc: {2}\n".format(self.name,
-        self.longDesc,self.shortDesc)
-        return "\nName: {0}\nlongDesc: {1}\nshortDesc: {2}\n".format(self.name,self.longDesc,self.shortDesc)
+                                                                   self.longDesc, self.shortDesc)
+
 
 class Living(StdObject):
-    
-    def __init__(self, name, longDesc = "", shortDesc = "", gender = "Attack Helicopter", race = "Zamboni"):
+
+    def __init__(self, name, longDesc="", shortDesc="", gender="Attack Helicopter", race="Zamboni"):
         super().__init__(name, longDesc, shortDesc)
         self.gender = gender
         self.race = race
 
     def __str__(self):
         return "Name: {0}\ngender: {1}\nrace: {2}\nlongDesc: {3}\n"\
-        "shortDesc: {4}\n".format(self.name,self.gender,self.race,self.longDesc,self.shortDesc)
+            "shortDesc: {4}\n".format(
+                self.name, self.gender, self.race, self.longDesc, self.shortDesc)
+
 
 class Item(StdObject):
 
-    def __init__(self, name,longDesc="", shortDesc="", ownership=""):
+    def __init__(self, name, longDesc="", shortDesc="", ownership=""):
         super().__init__(name, longDesc, shortDesc)
         self.ownership = ownership
 
     def __str__(self):
         return super().__str__()
 
+
 class Container(Item):
 
     def __init__(self, name, longDesc="", shortDesc=""):
         super().__init__(name, longDesc, shortDesc)
         self.inventory = []
-    
+
     def __str__(self):
         return "Container"
