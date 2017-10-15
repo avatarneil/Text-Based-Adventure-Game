9007bd6740a6f532fe0f0a2f5e0d2681c4671732
Zac Krasnow <zac@Zacs-MacBook-Pro.local>
2017-10-14 23:22:20 -0400

    Began implementing Item class and Container subclass

--- story.py
+++ story.py
@@ -3,17 +3,37 @@ import numpy
 
 class StdObject():
 
-    def __init__(self,name,longDesc="",shortDesc=""):
+    def __init__(self, name, longDesc="", shortDesc=""):
         self.name = name
         self.longDesc = longDesc
         self.shortDesc = shortDesc
     
     def __str__(self):
-        return "Name: {0}\nlongDesc: {1}\nshortDesc: {2}\n".format(self.name,self.longDesc,self.shortDesc)
+        return "\nName: {0}\nlongDesc: {1}\nshortDesc: {2}\n".format(self.name,self.longDesc,self.shortDesc)
 
 
-#class Living(StdObject):
+class Item(StdObject):
 
-test = StdObject("Zac","An absolute nerd","Nerd")
+    def __init__(self, name,longDesc="", shortDesc="", ownership=""):
+        super().__init__(name, longDesc, shortDesc)
+        self.ownership = ownership
 
-print(test)
+    def __str__(self):
+        return super().__str__()
+
+class Container(Item):
+
+    def __init__(self, name, longDesc="", shortDesc=""):
+        super().__init__(name, longDesc, shortDesc)
+        self.inventory = []
+    
+    def __str__(self):
+        return "Container"
+
+
+
+testStdObject = StdObject("Zac","An absolute nerd","Nerd")
+testItem = Item("Sword","a glowing magic sword","magic sword")
+
+print(testStdObject)
+print(testItem)
